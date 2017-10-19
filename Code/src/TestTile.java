9e47f121f2ca2a5c8de473d9e347de748f4e6321
Nicholas Walter <nickwalt@rams.colostate.edu>
2017-10-18 21:46:26 -0600

    token & tile test cases

--- Code/src/TestTile.java
+++ Code/src/TestTile.java
@@ -5,11 +5,15 @@ public class TestTile {
 
 	@Test
 	public void testConstructor() {
-		
+		Tile tile = new Tile(1,1);
+		assertNotEquals(null, tile);
 	}
 	
 	@Test
 	public void testGettersSetters() {
-		
+		Tile tile = new Tile(5,5);
+		assertArrayEquals(new int[]{5,5}, tile.getPosition());
+		tile.setToken(new Token(Type.SOLDIER, Color.BLACK));
+		assertEquals("B1", tile.getToken().abbreviate());
 	}
 }
