fa611fd56150af7f622d6fc720187fd85bb47049
Dumamax <dumamax@gmail.com>
2017-10-19 15:01:37 -0600

    Made moveToken() return a boolean stating whether it was successful or not

--- Code/src/Game.java
+++ Code/src/Game.java
@@ -92,21 +92,23 @@ public class Game {
 		}
 	}
 	
-	public void moveToken(int startX, int startY, int endX, int endY) {
+	public boolean moveToken(int startX, int startY, int endX, int endY) {
 		Token token = board.getToken(startX, startY);
 		if(isValidMove(startX, startY, endX, endY)) {
 			if(board.getToken(endX, endY) == null) {
 				board.getTile(endX, endY).setToken(token);
 				board.getTile(startX, startY).setToken(null);
+				return true;
 			}else {
 				Token token2 = board.getToken(endX, endY);
 				if(isValidAttack(token, token2)) {
 					board.moveToGraveyard(token2);
 					board.getTile(endX, endY).setToken(token);
 					board.getTile(startX, startY).setToken(null);
-				}
+					return true;
+				}else return false;
 			}
-		}
+		}else return false;
 	}
 	
 	public boolean isValidMove(int startX, int startY, int endX, int endY) {
@@ -115,7 +117,7 @@ public class Game {
 //			//TODO Deal with cannon movement here
 //		}else {
 			//Make sure the correct player is the one making the move here!
-			//Als make sure they are attempting to move the correct piece!
+			//Also make sure they are attempting to move the correct piece!
 			if(endX < 1 || endX > 8 || endY < 1 || endY > 4) {
 				return false;
 			}else if(Math.abs(endX-startX) > 1) {
