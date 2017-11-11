269c2b7c9e7231fe2c460141368af273f1b60c67
Nicholas Walter <nickwalt@rams.colostate.edu>
2017-11-11 10:16:23 -0700

    method stubs set up

--- Code/src/model/Invitation.java
+++ Code/src/model/Invitation.java
@@ -2,4 +2,16 @@ package model;
 
 public class Invitation {
 
+	private User sender, reciever;
+	private Status status;
+	private Game goesToGame;
+	
+	public void notifyReciever() {
+		
+	}
+	
+	public void accepted() {
+		
+	}
+	
 }
--- Code/src/model/User.java
+++ Code/src/model/User.java
@@ -1,12 +1,42 @@
 package model;
 
+import java.util.ArrayList;
+
 public class User {
 	
+	private String email, password;
+	private Profile profile;
+	private ArrayList<Game> games = new ArrayList<Game>();
+	private ArrayList<Invitation> invitations = new ArrayList<Invitation>();
 	
 	public User() {
 		//beaware that this constructor for User is used in testGettersSetters()
 		//in TestGame.java - if you change this constructor, that
 		//testing method may be broken. If it is, please fix it :) - Nicholas
+	}
+	
+	public Invitation createInvitation() {
+		return null;
+	}
+	
+	public void acceptInvitation() {
+		
+	}
+	
+	public Game createGame() {
+		return null;
+	}
+	
+	public void playGame() {
+		
+	}
+	
+	public void viewProfile() {
+		
+	}
+	
+	public void quitGame() {
+		
 	}
 	
 }
