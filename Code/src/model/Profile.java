b1ca609de07f17d213299a86ae5f0dac580c9cb0
Nicholas Walter <nickwalt@rams.colostate.edu>
2017-11-12 15:26:51 -0700

    profile

--- Code/src/model/Profile.java
+++ Code/src/model/Profile.java
@@ -5,7 +5,46 @@ import java.util.ArrayList;
 public class Profile {
 
 	private String nickname;
-	private ArrayList<Log> history = new ArrayList<Log>();
+	private ArrayList<Log> history;
 	private double winLossRatio;
+	private User user;
+	
+	public Profile(String nickname, ArrayList<Log> history, double winLossRatio, User user) {
+		this.nickname = nickname;
+		this.history = history;
+		this.winLossRatio = winLossRatio;
+		this.user = user;
+	}
+	
+	public void addEntry(Log log){
+		history.add(log);
+		computeWLR();
+	}
+	
+	public void computeWLR() {
+		double wins = 0.0, losses = 0.0;
+		for(Log log : history) {
+			
+			if(!log.isOver()) {
+				continue;
+			}
+			
+			if(log.isWinner(user)) {
+				wins++;
+			}else {
+				losses++;
+			}
+		}
+		
+		winLossRatio = wins/losses;
+	}
+	
+	public ArrayList<Log> getHistory(){
+		return history;
+	}
+	
+	public double getWinLossRatio() {
+		return winLossRatio;
+	}
 	
 }
