c89219de6cbd455a0ae6257196ae810548068276
twzlr <twzlr@ProStatus>
2017-11-13 12:30:31 -0700

    view profile window

--- Code/src/userInterface/ViewProfile.java
+++ Code/src/userInterface/ViewProfile.java
@@ -4,18 +4,22 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
+import model.User;
+import javax.swing.JTextPane;
+
 public class ViewProfile {
 
 	private JFrame frame;
+	private User user;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(User mUser) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ViewProfile window = new ViewProfile();
+					ViewProfile window = new ViewProfile(mUser);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -27,7 +31,8 @@ public class ViewProfile {
 	/**
 	 * Create the application.
 	 */
-	public ViewProfile() {
+	public ViewProfile(User aUser) {
+		this.user=aUser;
 		initialize();
 	}
 
@@ -36,8 +41,13 @@ public class ViewProfile {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 644, 457);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextPane profileText = new JTextPane();
+		profileText.setText(null);
+		profileText.setBounds(12, 13, 446, 384);
+		frame.getContentPane().add(profileText);
 	}
-
 }
